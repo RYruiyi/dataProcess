83269b5bb49db6d02ded7ac378a8977828dcd53e
pangpang <zry18829212180@163.com>
2018-04-21 16:57:22 +0800

    modified

--- python/csv2MysqlBatch.py
+++ python/csv2MysqlBatch.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 import MySQLdb
 
 conn=MySQLdb.connect('localhost','root','123456','pangpang')
 x = conn.cursor()
-path = "../data/new_all.csv"
+path = "../data/new.csv"
 sql = "INSERT INTO taobao VALUES (%s,%s,%s,%s)"
 
 try:
 	with open(path, 'r') as p:
 		batch_list = []
-		for line in p.readlines():
+		for line in p.readlines()[1:101]:
 			split_line = line.split(",")
-			user_id = line.split(",")[0]
-			age_range = line.split(",")[1]
-			gender = line.split(",")[2]
-			m_id = line.split(",")[3]
+			user_id = split_line[0]
+			age_range = split_line[1]
+			gender = split_line[2]
+			m_id = split_line[3]
 			seq = (user_id, age_range, gender, m_id)
 			batch_list.append(seq)
 		# 批量batch插入
